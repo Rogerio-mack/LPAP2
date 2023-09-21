293b43c83f5d8b156ec8959eae3e7270f01ff1d7
Rogerio de Oliveira <58958893+Rogerio-mack@users.noreply.github.com>
2023-09-20 21:56:33 -0300

    Create poli_schema.c

---
+++ poli_schema.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+// Função para ler um polinômio de grau n
+void lerPolinomio(int polinomio[], int grau) {
+// ...
+}
+
+// Função para somar dois polinômios
+void somarPolinomios(int polinomio1[], int grau1, int polinomio2[], int grau2, int resultado[]) {
+// ...
+}
+
+// Função para multiplicar dois polinômios
+void multiplicarPolinomios(int polinomio1[], int grau1, int polinomio2[], int grau2, int resultado[]) {
+// ...
+}
+
+// Função para avaliar um polinômio dado o valor de x
+int avaliarPolinomio(int polinomio[], int grau, int x) {
+// ...
+    return resultado;
+}
+
+// Função para imprimir um polinômio
+void imprimirPolinomio(int polinomio[], int grau) {
+// ...
+}
+
+// Função para obter o grau de um polinômio representado por um vetor (tamanho - 1)
+void imprimirPolinomio(int polinomio[], int grau) {
+  grau = sizeof(polinomio) / sizeof(polinomio[0]); 
+}
+
+
+int main() {
+// ...
+}
